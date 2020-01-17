a81fc6ea032767aef207408623a8717d842b6f4a
Shura_client <sh@gmail.com>
2020-01-17 11:59:08 +0400

    add comments

--- public/index.php
+++ public/index.php
@@ -1,18 +1,23 @@
 <?php
+/*
+Подключение  файлов
+*/
 require "../bootstrap.php";
 use Src\Ctrl\IssueCtrl;
 
+/*
+Установка заголовков http ответа
+*/
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+/*
+Получение и парсинг url запроса
+*/
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
-
-
-
 if ($uri[1] !== 'public') {
     header("HTTP/1.1 404 Not Found");
    echo json_encode( $uri[1]);
@@ -20,17 +25,21 @@ if ($uri[1] !== 'public') {
 }
 
 
-
+/*
+Получение id записи, если есть
+*/
 $issueid = null;
 if (isset($uri[2])) {
     $issueid = (int) $uri[2];
 }
 
+/*
+Получение Типа http запроса
+*/
 $requestMethod = $_SERVER["REQUEST_METHOD"];
-
-
-//
-
+/*
+Передача, соединения с БД, тип запроса и id и запуск обработчика (контроллера)
+*/
 $controller = new IssueCtrl($dbConn, $requestMethod, $issueid);
 $controller->processRequest();
 
